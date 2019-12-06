b0ea755a8f76c95011367252e4f5205247dfd065
Leon Skrilec <leon.skrilec@agencija101.si>
2019-12-06 11:09:21 +0100

    changes

--- index.js
+++ index.js
@@ -1,69 +1,86 @@
 const JsBarcode = require("jsbarcode");
-const { createCanvas, loadImage } = require("canvas");
+const { createCanvas } = require("canvas");
 const fs = require("fs");
-const codes = require("./codes.js");
-// JsBarcode(canvas, "Hello");
+const { DOMImplementation, XMLSerializer } = require('xmldom');
+
+const settings = {
+    inputFile: "./codes.txt",
+    outputFormat: "png", // svg || png
+    outputFolder: `./output/png/`,
+    codeSeparator: "\r\n", // separator which separates each code in inputFile
+}
 
 const state = {
     progress: 0,
-    canvases: {
-        bg: {
-            canvas: null,
-            ctx: null,
-        },
-        barcode: {
-            canvas: null,
-            ctx: null,
-        },
+    barcode: {
+        canvas: null,
+        ctx: null,
     },
-
-    bg: null,
-    codes,
+    codes: [],
+    svg: {
+        XMLSerializer: null,
+        document: null,
+        node: null
+    }
 };
 
-function start() {
+function generateBarcode() {
     console.log("progress", state.progress);
-    console.log("codes length", state.codes.codes.length);
-    if (state.progress === state.codes.codes.length) return false;
+    console.log("codes length", state.codes.length);
+    if (state.progress === state.codes.length) return false;
 
-    const code = state.codes.codes[state.progress];
+    const code = state.codes[state.progress];
     state.progress += 1;
 
     return saveImage(code);
 }
 
 function saveImage(code) {
-    JsBarcode(state.canvases.barcode.canvas, code, {
-        format: "CODE128",
-    });
+    if (settings.outputFormat === "png") {
+        JsBarcode(state.barcode.canvas, code, {
+            format: "CODE39",
+            height:120,
+            displayValue: false
+        });
+        
+        fs.writeFileSync(`${settings.outputFolder}${code}.png`, state.barcode.canvas.toBuffer());
+    }
+    else if (settings.outputFormat === "svg") {
+        JsBarcode(state.svg.node, code, {
+            xmlDocument: state.svg.document,
+        });
 
-    state.canvases.bg.ctx.drawImage(state.bg, 0, 0, 600, 600);
-    state.canvases.bg.ctx.drawImage(state.canvases.barcode.canvas, 145, 450, 310, 142);
+        const svgText = state.svg.XMLSerializer.serializeToString(state.svg.node);
+        fs.writeFileSync(`${settings.outputFolder}${state.progress}.svg`, svgText);
+    }
+    
 
-    fs.writeFileSync(`./output/${state.progress}.png`, state.canvases.bg.canvas.toBuffer());
-
-    return start();
+    return generateBarcode();
 }
 
 function initCanvas() {
-    state.canvases.bg.canvas = createCanvas(600, 600);
-    state.canvases.bg.ctx = state.canvases.bg.canvas.getContext("2d");
+    state.barcode.canvas = createCanvas(600, 600);
+    state.barcode.ctx = state.barcode.canvas.getContext("2d");
+}
 
-    state.canvases.barcode.canvas = createCanvas(600, 600);
-    state.canvases.barcode.ctx = state.canvases.barcode.canvas.getContext("2d");
+function initSvgDocument() {
+    state.svg.XMLSerializer = new XMLSerializer();
+    state.svg.document = new DOMImplementation().createDocument('http://www.w3.org/1999/xhtml', 'html', null);
+    state.svg.node = state.svg.document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 }
 
 function initData() {
-    loadImage("./bg.png")
-        .then(image => {
-            state.bg = image;
-            initCanvas();
-            start();
-        })
-        .catch(res => {
-            console.log(res);
-            throw new Error(res);
-        });
+    // Check if output folder exists - if not create it
+    if (!fs.existsSync(settings.outputFolder)) fs.mkdirSync(settings.outputFolder);
+
+    if (settings.outputFormat === "svg") initSvgDocument();
+    else if (settings.outputFormat === "png") initCanvas();
+    // Load data
+    fs.readFile(settings.inputFile, 'utf8', function(err, data) {
+        state.codes = data.split(settings.codeSeparator);
+        initCanvas();
+        generateBarcode();
+    });
 }
 
 initData();
